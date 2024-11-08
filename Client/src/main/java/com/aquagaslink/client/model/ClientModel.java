4dd847bf7960ae4eeb1875e564fcd712b47f5e5a
DK <daniel.cabral@smartnx.com>
2024-11-07 21:08:59 -0300

    criando serviço de client

---
+++ Client/src/main/java/com/aquagaslink/client/model/ClientModel.java
@@ -0,0 +1,105 @@
+package com.aquagaslink.client.model;
+
+import com.aquagaslink.client.controller.clientDTO.ClientDTO;
+import com.aquagaslink.client.controller.clientDTO.ClientDTOForm;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
+
+@Entity
+public class ClientModel {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String cpf;
+    private String name;
+    private String email;
+    private String phone;
+    private String address;
+    private String city;
+    private String state;
+    private String number;
+    private String country;
+
+    public ClientModel() {
+    }
+
+    public ClientModel(String cpf, String name, String email, String phone, String address, String city, String state, String number, String country) {
+        this.cpf = cpf;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.number = number;
+        this.country = country;
+    }
+
+    public ClientModel(ClientDTOForm clientDTOForm) {
+        this.cpf = clientDTOForm.cpf();
+        this.name = clientDTOForm.name();
+        this.email = clientDTOForm.email();
+        this.phone = clientDTOForm.phone();
+        this.address = clientDTOForm.address();
+        this.city = clientDTOForm.city();
+        this.state = clientDTOForm.state();
+        this.number = clientDTOForm.number();
+        this.country = clientDTOForm.country();
+    }
+
+    public void updateFromDTO(ClientDTOForm clientDTOForm) {
+        this.cpf = clientDTOForm.cpf();
+        this.name = clientDTOForm.name();
+        this.email = clientDTOForm.email();
+        this.phone = clientDTOForm.phone();
+        this.address = clientDTOForm.address();
+        this.city = clientDTOForm.city();
+        this.state = clientDTOForm.state();
+        this.number = clientDTOForm.number();
+        this.country = clientDTOForm.country();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+}
---
+++ Client/src/main/java/com/aquagaslink/client/service/ClientService.java
@@ -0,0 +1,59 @@
+package com.aquagaslink.client.service;
+
+import com.aquagaslink.client.controller.clientDTO.ClientDTO;
+import com.aquagaslink.client.controller.clientDTO.ClientDTOForm;
+import com.aquagaslink.client.infrastructure.ClientRepository;
+import com.aquagaslink.client.model.ClientModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class ClientService {
+
+    @Autowired
+    private ClientRepository clientRepository;
+
+
+    public ClientDTO createClient(ClientDTOForm clientDTOForm) {
+        var client = clientRepository.findByCpf(clientDTOForm.cpf());
+        if (client.isPresent()) {
+            throw new RuntimeException("Client already exists");
+        }
+        
+        var clientModel = clientRepository.save(new ClientModel(clientDTOForm));
+        return new ClientDTO(clientModel);
+    }
+
+    public List<ClientDTO> getAllClient() {
+        return clientRepository.findAll().stream().map(ClientDTO::new).toList();
+    }
+
+    public ClientDTO getClient(UUID id) {
+        var client = clientRepository.findById(id);
+        return client.map(ClientDTO::new).orElse(null);
+    }
+
+    public ClientDTO updateClient(UUID id, ClientDTOForm clientDTOForm) {
+    var client = clientRepository.findById(id);
+    if (client.isEmpty()) {
+        throw new RuntimeException("Client not found");
+    }
+    var clientModel = client.get();
+    clientModel.updateFromDTO(clientDTOForm);
+    clientRepository.save(clientModel);
+
+    return new ClientDTO(clientModel);
+}
+
+    public void deleteClient(UUID id) {
+        var client = clientRepository.findById(id);
+        if (client.isEmpty()) {
+            throw new RuntimeException("Client not found");
+        }
+        clientRepository.delete(client.get());
+
+    }
+}
