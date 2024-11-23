5cc7e111239e5a149f14685bad7c80313a8d3eff
Guilherme Lira <guilira.dev@gmail.com>
2024-11-22 21:37:57 -0300

    teste: tentativa 2

--- Product/src/main/java/com/aquagaslink/product/ProductApplication.java
+++ Product/src/main/java/com/aquagaslink/product/ProductApplication.java
@@ -13,30 +13,4 @@ public class ProductApplication {
 
         SpringApplication.run(ProductApplication.class, args);
     }
-    @Bean
-    public Function<String, String> productEventListener() {
-        return value -> {
-            // Lógica do listener de produto
-            System.out.println("Product Event: " + value);
-            return value;
-        };
-    }
-
-    @Bean
-    public Function<String, String> clientEventListener() {
-        return value -> {
-            // Lógica do listener de cliente
-            System.out.println("Client Event: " + value);
-            return value;
-        };
-    }
-
-    @Bean
-    public Function<String, String> orderEventListener() {
-        return value -> {
-            // Lógica do listener de pedido
-            System.out.println("Order Event: " + value);
-            return value;
-        };
-    }
 }
--- Product/src/main/java/com/aquagaslink/product/queue/consumer/ProductEventListener.java
+++ Product/src/main/java/com/aquagaslink/product/queue/consumer/ProductEventListener.java
@@ -1,15 +1,37 @@
-//package com.aquagaslink.product.queue.consumer;
-//
-//
-//
-//import org.springframework.context.annotation.Bean;
-//
-//import org.springframework.stereotype.Component;
-//
-//import java.util.function.Function;
-//
-//@Component
-//public class ProductEventListener {
-//
-//
-//}
+package com.aquagaslink.product.queue.consumer;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.function.Function;
+
+@Component
+public class ProductEventListener {
+
+    @Bean
+    public Function<String, String> productEventListener() {
+        return value -> {
+            // Lógica do listener de produto
+            System.out.println("Product Event: " + value);
+            return value;
+        };
+    }
+
+    @Bean
+    public Function<String, String> clientEventListener() {
+        return value -> {
+            // Lógica do listener de cliente
+            System.out.println("Client Event: " + value);
+            return value;
+        };
+    }
+
+    @Bean
+    public Function<String, String> orderEventListener() {
+        return value -> {
+            // Lógica do listener de pedido
+            System.out.println("Order Event: " + value);
+            return value;
+        };
+    }
+}
