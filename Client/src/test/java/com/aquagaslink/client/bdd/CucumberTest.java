6944eb8a602609761578d085900307d503935cee
KarenAss <karenadf2000@gmail.com>
2024-11-26 21:19:36 -0300

    Início BDD Client

---
+++ Client/src/test/java/com/aquagaslink/client/bdd/CucumberTest.java
@@ -0,0 +1,14 @@
+package com.aquagaslink.client.bdd;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features="src/test/resources/features/client.feature",
+        glue = "com.aquagaslink.client.bdd",
+        plugin = {"pretty", "html:target/cucumber-reports"}
+)
+public class CucumberTest {
+}
---
+++ Client/src/test/java/com/aquagaslink/client/helper/ClientHelper.java
@@ -0,0 +1,14 @@
+package com.aquagaslink.client.helper;
+
+import com.aquagaslink.client.controller.clientDTO.ClientDTOForm;
+
+public class ClientHelper {
+
+    public static ClientDTOForm createClient(String cpf){
+        return new ClientDTOForm(cpf,"Joao","Joao@teste.com","1198765433","Av. test","São Paulo", "SP", "123", "Brazil");
+    }
+
+    public static ClientDTOForm createClientData(String cpf, String name){
+        return new ClientDTOForm(cpf,name,"Joao@teste.com","1198765433","Av. test","São Paulo", "SP", "123", "Brazil");
+    }
+}
