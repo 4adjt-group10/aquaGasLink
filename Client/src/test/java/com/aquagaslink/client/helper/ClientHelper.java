ed488e413c63c9e1946809b871f93859109d7024
marcosMol <marcos_mol@hotmail.com>
2024-11-30 16:47:01 -0300

    correção em teste

--- Client/src/test/java/com/aquagaslink/client/helper/ClientHelper.java
+++ Client/src/test/java/com/aquagaslink/client/helper/ClientHelper.java
@@ -5,10 +5,10 @@ import com.aquagaslink.client.controller.clientDTO.ClientDTOForm;
 public class ClientHelper {
 
     public static ClientDTOForm createClient(String cpf){
-        return new ClientDTOForm(cpf,"Joao","Joao@teste.com","1198765433","Av. test","São Paulo", "SP", "123", "Brazil");
+        return new ClientDTOForm(cpf,"Joao","Joao@teste.com","1198765433","333145660","Av. test","São Paulo", "SP", "123", "Brazil");
     }
 
     public static ClientDTOForm createClientData(String cpf, String name){
-        return new ClientDTOForm(cpf,name,"Joao@teste.com","1198765433","Av. test","São Paulo", "SP", "123", "Brazil");
+        return new ClientDTOForm(cpf,name,"Joao@teste.com","1198765433","333145660","Av. test","São Paulo", "SP", "123", "Brazil");
     }
 }
