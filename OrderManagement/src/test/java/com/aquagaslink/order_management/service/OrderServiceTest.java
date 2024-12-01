3eb86fa1c424606ad4cf03aca5938c676a9d6520
DK <daniel.cabral@smartnx.com>
2024-11-30 21:18:04 -0300

    Refazendo os teste de order - 30% até momento

--- OrderManagement/src/test/java/com/aquagaslink/order_management/service/OrderServiceTest.java
+++ OrderManagement/src/test/java/com/aquagaslink/order_management/service/OrderServiceTest.java
@@ -1,54 +1,61 @@
-//package com.aquagaslink.order_management.service;
-//
-//import com.aquagaslink.order_management.controller.dto.OrderDto;
-//import com.aquagaslink.order_management.controller.dto.OrderFormDto;
-//import com.aquagaslink.order_management.helper.ClientOrderHelper;
-//import com.aquagaslink.order_management.infrastructure.ClientOrderRepository;
-//import com.aquagaslink.order_management.model.ClientOrder;
-//import com.aquagaslink.order_management.model.OrderStatus;
-//import org.junit.Assert;
-//import org.junit.jupiter.api.AfterEach;
-//import org.junit.jupiter.api.Assertions;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.Mock;
-//import org.mockito.MockitoAnnotations;
-//import org.springframework.data.domain.Page;
-//import org.springframework.data.domain.PageImpl;
-//import org.springframework.data.domain.PageRequest;
-//import org.springframework.data.domain.Pageable;
-//
-//import java.util.Arrays;
-//import java.util.Optional;
-//import java.util.UUID;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//import static org.junit.Assert.assertEquals;
-//import static org.mockito.ArgumentMatchers.any;
-//import static org.mockito.Mockito.*;
-//
-//public class OrderServiceTest {
-//
-//    @Mock
-//    private ClientOrderRepository clientOrderRepository;
-//
-//    private OrderService service;
-//
-//    private ClientOrderHelper helper;
-//
-//    AutoCloseable mock;
-//
-//    @BeforeEach
-//    void setup(){
-//        mock = MockitoAnnotations.openMocks(this);
-//        service = new OrderService(clientOrderRepository, null);
-//    };
-//
-//    @AfterEach
-//    void tearDown() throws Exception {
-//        mock.close();
-//    }
-//
+package com.aquagaslink.order_management.service;
+
+import com.aquagaslink.order_management.controller.dto.OrderDto;
+import com.aquagaslink.order_management.controller.dto.OrderFormDto;
+import com.aquagaslink.order_management.helper.ClientOrderHelper;
+import com.aquagaslink.order_management.infrastructure.ClientOrderRepository;
+import com.aquagaslink.order_management.model.ClientOrder;
+import com.aquagaslink.order_management.model.OrderStatus;
+import com.aquagaslink.order_management.queue.OrderEventGateway;
+import com.aquagaslink.order_management.queue.dto.client.ClientToOrderIn;
+import com.aquagaslink.order_management.queue.dto.product.OrderToProductOut;
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+public class OrderServiceTest {
+
+    @Mock
+    private ClientOrderRepository clientOrderRepository;
+
+    private OrderService service;
+
+    private ClientOrderHelper helper;
+
+    private OrderEventGateway orderEventGateway;
+
+    AutoCloseable mock;
+
+    @BeforeEach
+    void setup(){
+        mock = MockitoAnnotations.openMocks(this);
+        service = new OrderService(clientOrderRepository, null);
+    };
+
+    @AfterEach
+    void tearDown() throws Exception {
+        mock.close();
+    }
+
 //    @Test
 //    void shouldCreateOrder(){
 //        var code = "code";
@@ -66,87 +73,90 @@
 //        verify(clientOrderRepository, times(1)).save(any(ClientOrder.class));
 //
 //    }
-//
+
+    @Test
+    void shouldFindOrderById() {
+        UUID orderId = UUID.randomUUID();
+        ClientOrder clientOrder = mock(ClientOrder.class);
+        when(clientOrderRepository.findById(orderId)).thenReturn(Optional.of(clientOrder));
+        when(clientOrder.getId()).thenReturn(orderId);
+
+        OrderDto result = service.getOrderById(orderId);
+
+        assertNotNull(result);
+        assertEquals(orderId, result.id());
+        verify(clientOrderRepository, times(1)).findById(orderId);
+    }
+
+
+    @Test
+    void shouldListOrdersByClientId() {
+        UUID clientId = UUID.randomUUID();
+        Pageable pageable = PageRequest.of(0, 10);
+        ClientOrder order1 = mock(ClientOrder.class);
+        ClientOrder order2 = mock(ClientOrder.class);
+        Page<ClientOrder> page = new PageImpl<>(Arrays.asList(order1, order2), pageable, 2);
+
+        when(clientOrderRepository.findAllByClientId(clientId, pageable)).thenReturn(page);
+
+        Page<OrderDto> result = service.listOrdersByClientId(clientId, pageable);
+
+        assertNotNull(result);
+        assertEquals(2, result.getTotalElements());
+        verify(clientOrderRepository, times(1)).findAllByClientId(clientId, pageable);
+    }
+
+    @Test
+    void shouldUpdateOrder() {
+        UUID orderId = UUID.randomUUID();
+        OrderFormDto formDto = new OrderFormDto(UUID.randomUUID(), 1, BigDecimal.ONE, UUID.randomUUID());
+        ClientOrder clientOrder = mock(ClientOrder.class);
+
+        when(clientOrderRepository.findById(orderId)).thenReturn(Optional.of(clientOrder));
+        when(clientOrderRepository.save(clientOrder)).thenReturn(clientOrder);
+
+        OrderDto result = service.updateOrder(orderId, formDto);
+
+        assertNotNull(result);
+        verify(clientOrderRepository, times(1)).findById(orderId);
+        verify(clientOrderRepository, times(1)).save(clientOrder);
+    }
+
+    @Test
+    void shouldUpdateOrderStatus() {
+        UUID orderId = UUID.randomUUID();
+        OrderStatus newStatus = OrderStatus.COMPLETED;
+        ClientOrder clientOrder = mock(ClientOrder.class);
+
+        when(clientOrderRepository.findById(orderId)).thenReturn(Optional.of(clientOrder));
+        when(clientOrderRepository.save(clientOrder)).thenReturn(clientOrder);
+
+        OrderDto result = service.updateOrderStatus(orderId, newStatus);
+
+        assertNotNull(result);
+        verify(clientOrderRepository, times(1)).findById(orderId);
+        verify(clientOrderRepository, times(1)).save(clientOrder);
+        verify(clientOrder).setStatus(newStatus);
+    }
+
+    //todo - testando o metodo de validacao de cliente
 //    @Test
-//    void shouldFindOrderById(){
-//        var code = "code";
-//        ClientOrder existingOrder = helper.createClientOrder(code);
-//        OrderFormDto updateForm = helper.createOrderFormDto(); // Assuming OrderFormDto has update fields
-//        UUID orderId = existingOrder.getId();
+//    void shouldValidateClientWithError() {
+//        UUID orderId = UUID.randomUUID();
+//        UUID id = UUID.randomUUID();
+//        ClientToOrderIn payload = new ClientToOrderIn(id, orderId, "Client name", "Client phone", null, true, "Client error");
+//        ClientOrder clientOrder = mock(ClientOrder.class);
 //
-//        when(clientOrderRepository.findById(orderId)).thenReturn(Optional.of(existingOrder));
+//        when(clientOrderRepository.findById(orderId)).thenReturn(Optional.of(clientOrder));
 //
-//        var orderRead = service.getOrderById(orderId);
+//        service.validateClient(payload);
 //
-//        assertThat(orderRead.id()).isEqualTo(existingOrder.getId());
-//        assertThat(orderRead.code()).isEqualTo(existingOrder.getCode());
-//        assertThat(orderRead.status()).isEqualTo(existingOrder.getStatus());
-//        assertThat(orderRead.clientName()).isEqualTo(existingOrder.getClientName());
-//        assertThat(orderRead.productCode()).isEqualTo(existingOrder.getProductCode());
+//        verify(clientOrderRepository, times(1)).findById(orderId);
+//        verify(clientOrder, times(1)).setHasClientError(true);
+//        verify(clientOrder, times(1)).setObservation(anyString());
+//        verify(clientOrder, times(1)).setStatus(OrderStatus.ERROR);
+//        verify(clientOrder, times(1)).setUpdatedAt();
+//        verify(clientOrderRepository, times(1)).saveAndFlush(clientOrder);
 //    }
-//
-//    @Test
-//    public void shouldUpdateOrder() throws Exception {
-//        var code = "code";
-//        ClientOrder existingOrder = helper.createClientOrder(code);
-//        OrderFormDto updateForm = helper.createOrderFormDto(); // Assuming OrderFormDto has update fields
-//        UUID orderId = existingOrder.getId();
-//
-//        when(clientOrderRepository.findById(orderId)).thenReturn(Optional.of(existingOrder));
-//        OrderDto updatedOrder = new OrderDto(existingOrder); // Mock updated OrderDto
-//
-//        when(clientOrderRepository.save(existingOrder)).thenReturn(existingOrder);
-//
-//        OrderDto result = service.updateOrder(orderId, updateForm);
-//
-//        Assert.assertNotEquals(updatedOrder.status(), result.status());
-//        verify(clientOrderRepository).findById(orderId);
-//        verify(clientOrderRepository).save(existingOrder);
-//    }
-//
-//    @Test
-//    public void shouldUpdateOrderWithStatus() throws Exception {
-//        var code = "code";
-//        ClientOrder existingOrder = helper.createClientOrder(code);
-//        OrderFormDto updateForm = helper.createOrderFormDto(); // Assuming OrderFormDto has update fields
-//        UUID orderId = existingOrder.getId();
-//        OrderStatus newStatus = OrderStatus.COMPLETED; // Change this to your desired status
-//
-//        when(clientOrderRepository.findById(orderId)).thenReturn(Optional.of(existingOrder));
-//        OrderDto updatedOrder = new OrderDto(existingOrder); // Mock updated OrderDto
-//
-//        when(clientOrderRepository.save(existingOrder)).thenReturn(existingOrder);
-//
-//        OrderDto result = service.updateOrderStatus(orderId, newStatus);
-//
-//        assertEquals(updatedOrder.code(), result.code());
-//        verify(clientOrderRepository).findById(orderId);
-//        verify(clientOrderRepository).save(existingOrder);
-//    }
-//
-//    @Test
-//    public void testListOrdersByClientId() {
-//        UUID clientId = UUID.randomUUID();
-//        Pageable pageable = PageRequest.of(0, 10);
-//        var order1 = helper.createClientOrderIdClient(clientId);
-//        var order2 = helper.createClientOrderIdClient(clientId);
-//
-//        Page<ClientOrder> page = new PageImpl<>(Arrays.asList(order1, order2), pageable, 20);
-//
-//        when(clientOrderRepository.findAllByClientId(clientId, pageable)).thenReturn(page);
-//        Page<OrderDto> result = service.listOrdersByClientId(clientId, pageable);
-//
-//        verify(clientOrderRepository).findAllByClientId(clientId, pageable);
-//        System.out.println(result.get().toList());
-//        Assertions.assertEquals(2, result.getNumberOfElements());
-//        Assertions.assertEquals(2, result.getContent().size());
-//        // ... other assertions on the content of the OrderDto objects
-//    }
-//
-//    @Test
-//    public void testConstructor() {
-//        Assertions.assertNotNull(service);
-//
-//    }
-//
-//}
+
+}
